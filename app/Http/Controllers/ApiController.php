b80dd6ed1987eb0671b302a5c5942af407e7a2d2
jjuser0110 <163217224+jjuser0110@users.noreply.github.com>
2026-02-27 17:53:31 +0800

    aaa

---
+++ app/Http/Controllers/ApiController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+use Spatie\Browsershot\Browsershot;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
+use App\Models\User;
+use App\Models\UserBank;
+use App\Models\UserAngpao;
+use App\Models\UserBonus;
+use App\Models\UserFirstDeposit;
+use Carbon\Carbon;
+use Illuminate\Support\Facades\Log;
+
+class ApiController extends Controller
+{
+    public function motiondetect(Request $request)
+    {
+        Log::info('Motion detected: ' . $request->getContent());
+    }
+}
